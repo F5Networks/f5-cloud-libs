fce50fc803b13df4b7fb8fe966fa26ce93da39a2
Mike Levine <m.levine@f5.com>
2016-06-30 09:32:09 -0700

    Remove createServerAndPool

--- setup.js
+++ setup.js
@@ -4,40 +4,6 @@ var BigIp = require('./lib/bigIp');
 
 var bigIp;
 
-var createVirtualAndPool = function() {
-    var pool;
-
-    return bigIp.create(
-        '/tm/ltm/pool',
-        {
-            'name': 'myPool',
-            'members': [
-                { 'name': '192.168.100.1:80', 'description': 'test-member-1' },
-                { 'name': '192.168.100.2:80', 'description': 'test-member-2' },
-                { 'name': '192.168.100.3:80', 'description': 'test-member-3' },
-            ]
-        }
-    )
-    .then(function(response) {
-        pool = response;
-
-        return bigIp.create(
-            '/tm/ltm/virtual',
-            {
-                'name': 'myVirtual',
-                'pool': pool.fullPath,
-                'destination': '1.2.3.4:80'
-            }
-        );
-    })
-    .then(function(response) {
-        console.log("Created virtual: " + JSON.stringify(response, null, 4));
-    })
-    .catch(function(error) {
-        console.log("createVirtualAndPool failed: " + error.message);
-    });
-};
-
 var collect = function(val, collection) {
     collection.push(val);
     return collection;
