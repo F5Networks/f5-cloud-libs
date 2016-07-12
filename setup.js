847aa99e6e90ff04103e0ab3fbc49c1bba606d77
Mike Levine <m.levine@f5.com>
2016-07-12 09:28:43 -0700

    - Option for log file location - Close log file

--- setup.js
+++ setup.js
@@ -9,7 +9,8 @@ var globalSettings = {
 var dbVars = {};
 var modules = {};
 
-var logFile = fs.openSync('/var/log/setup.log', 'w');
+var logFileName = '/tmp/setup.log';
+var logFile;
 
 var previousOperationMessage;
 var bigIp;
@@ -37,128 +38,140 @@ options
     .option('-g, --global-settings <name: value>', 'A global setting name/value pair. For multiple settings, use multiple -g entries', map, globalSettings)
     .option('-d, --db <name: value>', 'A db variable name/value pair. For multiple settings, use multiple -d entries', map, dbVars)
     .option('-m, --module <name: value>', 'A module provisioning module/level pair. For multiple modules, use multiple -m entries', map, modules)
-    .option('-f, --foreground', 'Do the work - otherwise spawn a background process to do the work.')
+    .option('-f, --foreground', 'Do the work - otherwise spawn a background process to do the work. If you are running in cloud init, you probably do not want this option.')
+    .option('-o, --output <file>', 'Full path for log file if background process is spawned. Default is ' + logFileName)
     .parse(process.argv);
 
-console.log(process.argv[1] + " called with" + process.argv.slice().join(" "));
+logFileName = options.output || logFileName;
 
-if (!options.foreground) {
+try {
+    logFile = fs.openSync(logFileName, 'a');
 
-    if (process.argv.length > 100) {
-        console.log("Too many arguments - maybe we're stuck in a restart loop?");
-    }
-    else {
-        console.log("Spawning child process to do the work.");
-        args = process.argv.slice(1);
-        args.push('--foreground');
-        myChild = childProcess.spawn(
-            process.argv[0],
-            args,
-            {
-                detached: true,
-                stdio: ['ignore', logFile, logFile]
-            }
-        );
-        myChild.unref();
-    }
+    console.log(process.argv[1] + " called with" + process.argv.slice().join(" "));
 
-    process.exit();
-}
+    // When running in cloud init, we need to exit so that cloud init can complete and
+    // allow the Big-IP services to start
+    if (!options.foreground) {
 
-bigIp = new BigIp(options.host, options.user, options.password);
-
-console.log("Setup starting at: " + new Date().toUTCString());
-console.log("Waiting for BIG-IP to be ready...");
-bigIp.ready(60, 10000) // 10 minutes
-    .then(function() {
-        console.log("BIG-IP is ready.");
-
-        var nameServers = ["10.133.20.70", "10.133.20.71"];
-        var timezone = 'UTC';
-        var ntpServers = ["0.us.pool.ntp.org", "1.us.pool.ntp.org"];
-
-        var initialConfig = {
-            dns: {
-                nameServers: nameServers
-            },
-            ntp: {
-                timezone: timezone,
-                servers: ntpServers
-            },
-            hostname: options.hostName,
-            globalSettings: globalSettings
-        };
-
-        if (Object.keys(initialConfig).length) {
-            console.log("Performing initial setup...");
-            previousOperationMessage = "Initial setup complete";
-            return bigIp.initialSetup(initialConfig);
+        if (process.argv.length > 100) {
+            console.log("Too many arguments - maybe we're stuck in a restart loop?");
         }
         else {
-            return q();
-        }
-    })
-    .then(function() {
-        if (previousOperationMessage) {
-            console.log(previousOperationMessage);
-            previousOperationMessage = '';
-        }
-
-        if (Object.keys(dbVars).length > 0) {
-            console.log("Setting DB vars");
-            previousOperationMessage = "Db vars set";
-            return bigIp.setDbVars(dbVars);
-        }
-        else {
-            return q();
-        }
-    })
-    .then(function() {
-        if (previousOperationMessage) {
-            console.log(previousOperationMessage);
-            previousOperationMessage = '';
-        }
-
-        var registrationKey = options.license;
-        var addOnKeys = options.addOn;
-
-        if (registrationKey || addOnKeys.length > 0) {
-            console.log("Licensing...");
-
-            return bigIp.license(
+            console.log("Spawning child process to do the work. Output will be in " + logFileName);
+            args = process.argv.slice(1);
+            args.push('--foreground');
+            myChild = childProcess.spawn(
+                process.argv[0],
+                args,
                 {
-                    registrationKey: registrationKey,
-                    addOnKeys: addOnKeys
+                    detached: true,
+                    stdio: ['ignore', logFile, logFile]
                 }
             );
+            myChild.unref();
         }
 
-        return q();
-    })
-    .then(function(response) {
-        if (response) {
-            console.log(response);
-        }
+        process.exit();
+    }
 
-        if (Object.keys(modules).length > 0) {
-            console.log("Provisioning modules: " + JSON.stringify(modules, null, 4));
-            return bigIp.provision(modules);
-        }
-        else {
+    bigIp = new BigIp(options.host, options.user, options.password);
+
+    console.log("Setup starting at: " + new Date().toUTCString());
+    console.log("Waiting for BIG-IP to be ready...");
+    bigIp.ready(60, 10000) // 10 minutes
+        .then(function() {
+            console.log("BIG-IP is ready.");
+
+            var nameServers = ["10.133.20.70", "10.133.20.71"];
+            var timezone = 'UTC';
+            var ntpServers = ["0.us.pool.ntp.org", "1.us.pool.ntp.org"];
+
+            var initialConfig = {
+                dns: {
+                    nameServers: nameServers
+                },
+                ntp: {
+                    timezone: timezone,
+                    servers: ntpServers
+                },
+                hostname: options.hostName,
+                globalSettings: globalSettings
+            };
+
+            if (Object.keys(initialConfig).length) {
+                console.log("Performing initial setup...");
+                previousOperationMessage = "Initial setup complete";
+                return bigIp.initialSetup(initialConfig);
+            }
+            else {
+                return q();
+            }
+        })
+        .then(function() {
+            if (previousOperationMessage) {
+                console.log(previousOperationMessage);
+                previousOperationMessage = '';
+            }
+
+            if (Object.keys(dbVars).length > 0) {
+                console.log("Setting DB vars");
+                previousOperationMessage = "Db vars set";
+                return bigIp.setDbVars(dbVars);
+            }
+            else {
+                return q();
+            }
+        })
+        .then(function() {
+            if (previousOperationMessage) {
+                console.log(previousOperationMessage);
+                previousOperationMessage = '';
+            }
+
+            var registrationKey = options.license;
+            var addOnKeys = options.addOn;
+
+            if (registrationKey || addOnKeys.length > 0) {
+                console.log("Licensing...");
+
+                return bigIp.license(
+                    {
+                        registrationKey: registrationKey,
+                        addOnKeys: addOnKeys
+                    }
+                );
+            }
+
             return q();
-        }
-    })
-    .then(function(response) {
-        if (response) {
-            console.log(response);
-        }
+        })
+        .then(function(response) {
+            if (response) {
+                console.log(response);
+            }
 
-        console.log("BIG-IP setup complete.");
-    })
-    .catch(function(err) {
-        console.log("BIG-IP setup failed: " + (typeof err === 'object' ? err.message : err));
-    })
-    .done(function() {
-        console.log("Setup finished at: " + new Date().toUTCString());
-    });
+            if (Object.keys(modules).length > 0) {
+                console.log("Provisioning modules: " + JSON.stringify(modules, null, 4));
+                return bigIp.provision(modules);
+            }
+            else {
+                return q();
+            }
+        })
+        .then(function(response) {
+            if (response) {
+                console.log(response);
+            }
+
+            console.log("BIG-IP setup complete.");
+        })
+        .catch(function(err) {
+            console.log("BIG-IP setup failed: " + (typeof err === 'object' ? err.message : err));
+        })
+        .done(function() {
+            console.log("Setup finished at: " + new Date().toUTCString());
+        });
+}
+finally {
+    fs.closeSync(logFile);
+}
 
