4159672f387ed8ba27a5b73606a1f770e1e419b5
Mike Levine <m.levine@f5.com>
2016-07-22 14:26:12 -0700

    Install from zipped file on Azure storage
    Until an updated version of node-icontrol is in npm and
    f5-cloud-libs is in npm, we need to install everything this
    way or else deal with all of the dpeendencies.

--- azure/runSetup.sh
+++ azure/runSetup.sh
@@ -5,27 +5,15 @@
 # commands is to put them in a script and run the script.
 # This is that script.
 
-# This script expects that both the gzipped f5-cloud-libs directory AND this
-# script on its own (f5-cloud-libs/azure/runSetup.sh) have been downloaded
-# to the current directory.
-
-# When pulling from github, the gzipped f5-clod-libs will be of the form
-# f5-cloud-libs-<branch>, so branch should be the first argument to this script.
-# The rest of the arguments are passed to setup.js as is.
+# The arguments to this script are passed to setup.js as is.
 
 # Example:
-# runSetup.sh develop --host 127.0.0.1 --user admin --license ...
+# runSetup.sh --host 127.0.0.1 --user admin --license ...
 
-# If we get node-icontrol and f5-cloud-libs in npm, then this part won't be necessary...
-npm install commander
-npm install q
-pushd node_modules > /dev/null
-curl -o node-icontrol.tar.gz --location https://github.com/seattlevine/node-icontrol/archive/develop.tar.gz
-tar -xzf node-icontrol.tar.gz
-mv node-icontrol-develop icontrol
-popd > /dev/null
+# If our mods to node-icontrol are pushed to npm, we can push
+# f5-cloud-libs to npm as well and use npm here instead
+curl -o f5-cloud-libs.tar.gz https://f5cloudlibs.blob.core.windows.net/archive/f5-cloud-libs.tar.gz
 
-tar -xzf $1.tar.gz
-cd f5-cloud-libs-$1
-shift
-f5-rest-node setup.js "$*"
+tar -xzf f5-cloud-libs.tar.gz
+cd f5-cloud-libs
+f5-rest-node setup.js "$@"
