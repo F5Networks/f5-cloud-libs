15f6b6cacec4d32b4033af4b633a2e376f8820f2
Mike Levine <m.levine@f5.com>
2016-08-08 13:26:19 -0700

    Clustering initial commit

--- scripts/cluster.js
+++ scripts/cluster.js
@@ -13,11 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use-strict';
-
 (function() {
 
-    var options;
+    var DEFAULT_LOG_FILE = '/tmp/cluster.log';
+
+    var options = require('commander');
+    var globalSettings = {
+        guiSetup: 'disabled'
+    };
     var runner;
 
     module.exports = runner = {
@@ -31,208 +34,131 @@
          * @param {Function} cb - Optional cb to call when done
          */
         run: function(argv, testOpts, cb) {
+
+            var fs = require('fs');
+            var q = require('q');
             var BigIp = require('../lib/bigIp');
-            var Logger = require('../lib/logger');
-            var ActiveError = require('../lib/activeError');
-            var ipc = require('../lib/ipc');
-            var signals = require('../lib/signals');
             var util = require('../lib/util');
-            var loggerOptions = {};
-            var loggableArgs;
-            var logger;
+            var logFile;
             var logFileName;
             var bigIp;
-            var forceReboot;
-            var i;
 
-            var DEFAULT_LOG_FILE = '/tmp/cluster.log';
-            var ARGS_FILE_ID = 'cluster';
-            var KEYS_TO_MASK = ['-p', '--password', '--remote-password'];
-            var REQUIRED_OPTIONS = ['host', 'user', 'password'];
-
-            options = require('./commonOptions');
             testOpts = testOpts || {};
 
-            try {
-                options = options.getCommonOptions(DEFAULT_LOG_FILE)
-                    .option('--config-sync-ip <config_sync_ip>', 'IP address for config sync.')
-                    .option('--create-group', 'Create a device group with the options:')
-                    .option('    --device-group <device_group>', '    Name of the device group.')
-                    .option('    --sync-type <sync_type>', '    Type of sync this cluster is for ("sync-only" | "sync-failover").')
-                    .option('    --device <device_name>', '    A device name to add to the group. For multiple devices, use multiple --device entries.', util.collect, [])
-                    .option('    --auto-sync', '    Enable auto sync.')
-                    .option('    --save-on-auto-sync', '    Enable save on sync if auto sync is enabled.')
-                    .option('    --full-load-on-sync', '    Enable full load on sync.')
-                    .option('    --asm-sync', '    Enable ASM sync.')
-                    .option('    --network-failover', '    Enable network failover.')
-                    .option('--join-group', 'Join a remote device group with the options:')
-                    .option('    --remote-host <remote_ip_address>', '    Managemnt IP for the BIG-IP on which the group exists.')
-                    .option('    --remote-user <remote_user', '    Remote BIG-IP admin user name.')
-                    .option('    --remote-password <remote_password>', '    Remote BIG-IP admin user password.')
-                    .option('    --remote-port <remote_port>', '    Remote BIG-IP port to connect to. Default is port of this BIG-IP.', parseInt)
-                    .option('    --device-group <remote_device_group_name>', '    Name of existing device group on remote BIG-IP to join.')
-                    .option('    --sync', '    Tell the remote to sync to us after joining the group.')
-                    .option('--remove-from-cluster', 'Remove a device from the cluster')
-                    .option('    --device-group <device_group>', '    Name of the device group.')
-                    .option('    --device <device_name>', '    Device name to remove.')
-                    .parse(argv);
-
-                options.port = options.port || 443;
-
-                loggerOptions.console = true;
-                loggerOptions.logLevel = options.logLevel;
-
-                if (options.output) {
-                    loggerOptions.fileName = options.output;
-                }
-
-                logger = Logger.getLogger(loggerOptions);
-                util.logger = logger;
-
-                for (i = 0; i < REQUIRED_OPTIONS.length; ++i) {
-                    if (!options[REQUIRED_OPTIONS[i]]) {
-                        logger.error(REQUIRED_OPTIONS[i], "is a required command line option.");
-                        return;
+            var writeOutput = function(message) {
+                if (options.verbose || !options.silent) {
+                    message += '\n';
+                    if (logFile) {
+                        logFile.write(message);
+                    }
+                    else {
+                        process.stdout.write(message);
                     }
                 }
+            };
 
-                // When running in cloud init, we need to exit so that cloud init can complete and
-                // allow the BIG-IP services to start
-                if (options.background) {
-                    logFileName = options.output || DEFAULT_LOG_FILE;
-                    logger.info("Spawning child process to do the work. Output will be in", logFileName);
-                    util.runInBackgroundAndExit(process, logFileName);
+            var writeResponse = function(response) {
+                if (response && options.verbose) {
+                    writeOutput((typeof response === 'object' ? JSON.stringify(response, null, 4) : "  " + response));
                 }
+            };
 
-                // Log the input, but don't log passwords
-                loggableArgs = argv.slice();
-                for (i = 0; i < loggableArgs.length; ++i) {
-                    if (KEYS_TO_MASK.indexOf(loggableArgs[i]) !== -1) {
-                        loggableArgs[i + 1] = "*******";
+            options.reboot = true;
+            options
+                .option('--host <ip_address>', 'Current BIG-IP management IP.')
+                .option('-u, --user <user>', 'Current BIG-IP admin user.')
+                .option('-p, --password <password>', 'Current BIG-IP admin user password.')
+                .option('--device-group <device_group_name>', 'Name of the device group to use for sync.')
+                .option('--sync-type <sync_type>', 'Type of sync this cluster is for ("sync-only" | "sync-failover").')
+                .option('--device <device_name>', 'A device name to add to the group. For devices, use multiple --device entries.', util.collect, [])
+                .option('--autoSync', 'Turn on auto sync.')
+                .option('--saveOnAutoSync', 'Save on sync if auto sync is enabled.')
+                .option('--fullLoadOnSync', 'Turn on full load on sync.')
+                .option('--asmSync', 'Turn on ASM sync.')
+                .option('--networkFailover', 'Turn on network failover.')
+                .option('--no-reboot', 'Skip reboot even if it is recommended.')
+                .option('-f, --foreground', 'Do the work in the foreground - otherwise spawn a background process to do the work. If you are running in cloud init, you probably do not want this option.')
+                .option('--signal <pid>', 'Process ID to send USR1 to when clustering is complete (but before rebooting if we are rebooting).')
+                .option('-o, --output <file>', 'Full path for log file if background process is spawned. Default is ' + DEFAULT_LOG_FILE)
+                .option('--silent', 'Turn off all output.')
+                .option('--verbose', 'Turn on verbose output (overrides --silent).')
+                .parse(argv);
+
+            logFileName = options.output || DEFAULT_LOG_FILE;
+
+            // When running in cloud init, we need to exit so that cloud init can complete and
+            // allow the BIG-IP services to start
+            if (!options.foreground) {
+                writeOutput("Spawning child process to do the work. Output will be in " + logFileName);
+                util.runInBackgroundAndExit(process, logFileName);
+            }
+
+            if (options.output) {
+                logFile = fs.createWriteStream(logFileName);
+            }
+
+            writeOutput(process.argv[1] + " called with " + process.argv.slice().join(" "));
+
+            if (!options.deviceGroup || !options.syncType) {
+                throw new Error("deviceGroup and syncType are both required");
+            }
+
+            // Create the bigIp client object
+            bigIp = testOpts.bigIp || new BigIp(options.host, options.user, options.password);
+
+            // Start processing...
+            writeOutput("Cluster starting at: " + new Date().toUTCString());
+            writeOutput("Waiting for BIG-IP to be ready.");
+            bigIp.ready()
+                .then(function() {
+                    var deviceGroupOptions = {
+                        autoSync: options.autoSync,
+                        saveOnAutoSync: options.saveOnAutoSync,
+                        fullLoadOnSync: options.fullLoadOnSync,
+                        asmSync: options.asmSync,
+                        networkFailover: options.networkFailover
+                    };
+
+                    writeOutput("BIG-IP is ready.");
+
+                    if (options.deviceGroup && options.syncType) {
+                        return bigIp.cluster.createDeviceGroup(options.deviceGroup, options.syncType, options.device, deviceGroupOptions);
                     }
-                }
-                logger.info(loggableArgs[1] + " called with", loggableArgs.join(' '));
+                    else {
+                        return q();
+                    }
+                })
+                .then(function(response) {
+                    writeResponse(response);
+                })
+                .catch(function(err) {
+                    writeOutput("BIG-IP cluster failed: " + (typeof err === 'object' ? err.message : err));
+                })
+                .done(function() {
+                    writeOutput("Cluster finished at: " + new Date().toUTCString());
 
-                // Create the bigIp client object
-                bigIp = testOpts.bigIp || new BigIp(options.host,
-                                                    options.user,
-                                                    options.password,
-                                                    {
-                                                        port: options.port,
-                                                        logger: logger
-                                                    });
-
-                // Start processing...
-
-                // Save args in restart script in case we need to reboot to recover from an error
-                util.saveArgs(argv, ARGS_FILE_ID)
-                    .then(function() {
-                        if (options.waitFor) {
-                            logger.info("Waiting for", options.waitFor);
-                            return ipc.once(options.waitFor);
+                    if (options.signal) {
+                        writeOutput("Signalling " + options.signal);
+                        try {
+                            process.kill(options.signal, 'SIGUSR1');
                         }
-                    })
-                    .then(function() {
-                        logger.info("Cluster starting.");
-                        ipc.send(signals.CLUSTER_RUNNING);
-
-                        logger.info("Waiting for BIG-IP to be ready.");
-                        return bigIp.ready();
-                    })
-                    .then(function() {
-                        logger.info("BIG-IP is ready.");
-
-                        if (options.configSyncIp) {
-                            logger.info("Setting config sync ip.");
-                            return bigIp.cluster.configSyncIp(options.configSyncIp);
+                        catch (err) {
+                            writeOutput("Signal failed: " + err.message);
                         }
-                    })
-                    .then(function() {
-                        var deviceGroupOptions = {};
+                    }
 
-                        if (options.createGroup) {
-                            if (!options.deviceGroup || !options.syncType) {
-                                throw new Error('When creating a device group, both device-group and sync-type are required.');
-                            }
+                    if (logFile) {
+                        logFile.end();
+                    }
 
-                            logger.info("Creating group", options.deviceGroup + ".");
-                            deviceGroupOptions = {
-                                autoSync: options.autoSync,
-                                saveOnAutoSync: options.saveOnAutoSync,
-                                fullLoadOnSync: options.fullLoadOnSync,
-                                asmSync: options.asmSync,
-                                networkFailover: options.networkFailover
-                            };
+                    if (cb) {
+                        cb();
+                    }
+                });
+        },
 
-                            return bigIp.cluster.createDeviceGroup(options.deviceGroup, options.syncType, options.device, deviceGroupOptions);
-                        }
-                    })
-                    .then(function(response) {
-                        logger.debug(response);
-
-                        if (options.joinGroup) {
-                            logger.info("Joining group.");
-                            return bigIp.cluster.joinCluster(options.deviceGroup,
-                                                             options.remoteHost,
-                                                             options.remoteUser,
-                                                             options.remotePassword,
-                                                             {
-                                                                remotePort: options.remotePort,
-                                                                sync: options.sync
-                                                             });
-                        }
-                    })
-                    .then(function(response) {
-                        logger.debug(response);
-
-                        if (options.removeFromCluster) {
-                            logger.info("Removing", options.device, "from", options.deviceGroup);
-                            return bigIp.cluster.removeFromCluster(options.device,
-                                                                   options.deviceGroup);
-                        }
-                    })
-                    .then(function(response) {
-                        logger.debug(response);
-                        logger.info("Waiting for BIG-IP to be active.");
-                        return bigIp.active();
-                    })
-                    .then(function(response) {
-                        logger.debug(response);
-                        ipc.send(options.signal || signals.CLUSTER_DONE);
-                    })
-                    .catch(function(err) {
-                        logger.error("BIG-IP cluster failed", err);
-
-                        if (err instanceof ActiveError) {
-                            logger.warn("BIG-IP active check failed. Preparing for reboot.");
-                            forceReboot = util.prepareArgsForReboot();
-                        }
-                    })
-                    .done(function(response) {
-                        logger.debug(response);
-                        logger.info("Cluster finished.");
-
-                        if (forceReboot) {
-                            logger.warn("Rebooting.");
-                            bigIp.reboot();
-                        }
-                        else {
-                            util.deleteArgs(ARGS_FILE_ID);
-                        }
-
-                        if (cb) {
-                            cb();
-                        }
-                    });
-            }
-            catch (err) {
-                if (logger) {
-                    logger.error("Clustering error:", err);
-                }
-                else {
-                    console.log("Clustering error:", err);
-                }
-            }
+        getGlobalSettings: function() {
+            return globalSettings;
         },
 
         getOptions: function() {
