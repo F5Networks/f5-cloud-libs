b19b78b324645033c219e829033df2d6a983a093
Mike Levine <m.levine@f5.com>
2016-10-11 16:14:26 -0700

    Exclude unnecessary files from tarball

--- scripts/azure/deploy.sh
+++ scripts/azure/deploy.sh
@@ -9,7 +9,7 @@ fi
 
 echo GOT ENVIRONMENT $ENVIRONMENT
 pushd ../
-tar --exclude=".git" -zcvf f5-cloud-libs.tar.gz f5-cloud-libs
+tar --exclude=".git*" --exclude="test" --exclude="doc" --exclude="nodeunit" --exclude="jshint" -zcvf f5-cloud-libs.tar.gz f5-cloud-libs
 azure storage blob upload --quiet f5-cloud-libs.tar.gz $ENVIRONMENT f5-cloud-libs.tar.gz
 azure storage blob upload --quiet f5-cloud-libs/scripts/azure/runScripts.js $ENVIRONMENT runScripts.js
 azure storage blob upload --quiet f5-cloud-libs/scripts/deployHttp.sh $ENVIRONMENT deployHttp.sh
