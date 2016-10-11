0a0ae981de261bf5d1803315047855bc2bc4ad4c
Mike Levine <m.levine@f5.com>
2016-10-11 15:59:21 -0700

    Parameterize environment

--- scripts/azure/deploy.sh
+++ scripts/azure/deploy.sh
@@ -1,6 +1,13 @@
+# Single argument should be 'dev' or 'archive'. Default is 'archive'.
+
+ENVIRONMENT=archive
+if [ -n $1 ]; then
+    ENVIRONMENT=$1
+fi
+
 pushd ../
 tar --exclude=".git" -zcvf f5-cloud-libs.tar.gz f5-cloud-libs
-azure storage blob upload --quiet f5-cloud-libs.tar.gz archive f5-cloud-libs.tar.gz
-azure storage blob upload --quiet f5-cloud-libs/scripts/azure/runScripts.js archive runScripts.js
-azure storage blob upload --quiet f5-cloud-libs/scripts/deployHttp.sh archive deployHttp.sh
+azure storage blob upload --quiet f5-cloud-libs.tar.gz $ENVIRONMENT f5-cloud-libs.tar.gz
+azure storage blob upload --quiet f5-cloud-libs/scripts/azure/runScripts.js $ENVIRONMENT runScripts.js
+azure storage blob upload --quiet f5-cloud-libs/scripts/deployHttp.sh $ENVIRONMENT deployHttp.sh
 popd
