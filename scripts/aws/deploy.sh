276c2c44c99ef868180051f95c731787075804a7
Mike Levine <m.levine@f5.com>
2016-10-18 10:27:56 -0700

    Add dev option

--- scripts/aws/deploy.sh
+++ scripts/aws/deploy.sh
@@ -1,4 +1,24 @@
+#!/bin/bash
+
+# Single argument should be 'dev' if you want the dev environment.
+
+if [ -n "$1" ]; then
+    ENVIRONMENT=$1
+fi
+
+if [ -n "$ENVIRONMENT" ] && [ "$ENVIRONMENT" != "dev" ]; then
+    echo dev is the only option allowed. For prod environment, do not use an argument
+    exit 1
+fi
+
+DIRECTORY='f5-cloud-libs'
+if [ -n "$ENVIRONMENT" ]; then
+    DIRECTORY=$DIRECTORY-$ENVIRONMENT
+fi
+
+echo Pushing to $DIRECTORY
+
 pushd ../
 tar --exclude=".git*" --exclude="test" --exclude="doc" --exclude="nodeunit" --exclude="jshint" -zcvf f5-cloud-libs.tar.gz f5-cloud-libs
-aws s3 cp f5-cloud-libs.tar.gz s3://f5-cloud-libs/f5-cloud-libs.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp f5-cloud-libs.tar.gz s3://$DIRECTORY/f5-cloud-libs.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 popd
