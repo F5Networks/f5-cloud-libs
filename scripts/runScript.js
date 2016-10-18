f937553a93dcbf764c04871fbf8ad29dd84dcb1a
Mike Levine <m.levine@f5.com>
2016-10-18 12:58:08 -0700

    Save args for reboot

--- scripts/runScript.js
+++ scripts/runScript.js
@@ -18,6 +18,7 @@
 (function() {
 
     var DEFAULT_LOG_FILE = '/tmp/runScript.log';
+    var ARGS_FILE_ID = 'runScript';
 
     var options = require('commander');
     var runner;
@@ -46,125 +47,136 @@
 
             testOpts = testOpts || {};
 
-            options
-                .option('--background', 'Spawn a background process to do the work. If you are running in cloud init, you probably want this option.')
-                .option('-f, --file <script>', 'File name of script to run.')
-                .option('-u, --url <url>', 'URL from which to download script to run. This will override --file.')
-                .option('--cl-args <command_line_args>', 'String of arguments to send to the script as command line arguments.')
-                .option('--signal <signal>', 'Signal to send when done. Default SCRIPT_DONE.')
-                .option('--wait-for <signal>', 'Wait for the named signal before running.')
-                .option('--cwd <directory>', 'Current working directory for the script to run in.')
-                .option('--log-level <level>', 'Log level (none, error, warn, info, verbose, debug, silly). Default is info.', 'info')
-                .option('-o, --output <file>', 'Log to file as well as console. This is the default if background process is spawned. Default is ' + DEFAULT_LOG_FILE)
-                .parse(argv);
+            try {
+                options
+                    .option('--background', 'Spawn a background process to do the work. If you are running in cloud init, you probably want this option.')
+                    .option('-f, --file <script>', 'File name of script to run.')
+                    .option('-u, --url <url>', 'URL from which to download script to run. This will override --file.')
+                    .option('--cl-args <command_line_args>', 'String of arguments to send to the script as command line arguments.')
+                    .option('--signal <signal>', 'Signal to send when done. Default SCRIPT_DONE.')
+                    .option('--wait-for <signal>', 'Wait for the named signal before running.')
+                    .option('--cwd <directory>', 'Current working directory for the script to run in.')
+                    .option('--log-level <level>', 'Log level (none, error, warn, info, verbose, debug, silly). Default is info.', 'info')
+                    .option('-o, --output <file>', 'Log to file as well as console. This is the default if background process is spawned. Default is ' + DEFAULT_LOG_FILE)
+                    .parse(argv);
 
-            options.port = options.port || 443;
+                options.port = options.port || 443;
 
-            loggerOptions.console = true;
-            loggerOptions.logLevel = options.logLevel;
+                loggerOptions.console = true;
+                loggerOptions.logLevel = options.logLevel;
 
-            if (options.output) {
-                loggerOptions.fileName = options.output;
-            }
+                if (options.output) {
+                    loggerOptions.fileName = options.output;
+                }
 
-            logger = Logger.getLogger(loggerOptions);
+                logger = Logger.getLogger(loggerOptions);
 
-            // When running in cloud init, we need to exit so that cloud init can complete and
-            // allow the BIG-IP services to start
-            if (options.background) {
-                logFileName = options.output || DEFAULT_LOG_FILE;
-                logger.info("Spawning child process to do the work. Output will be in", logFileName);
-                util.runInBackgroundAndExit(process, logFileName);
-            }
+                // When running in cloud init, we need to exit so that cloud init can complete and
+                // allow the BIG-IP services to start
+                if (options.background) {
+                    logFileName = options.output || DEFAULT_LOG_FILE;
+                    logger.info("Spawning child process to do the work. Output will be in", logFileName);
+                    util.runInBackgroundAndExit(process, logFileName);
+                }
 
-            // Log the input, but don't log passwords
-            logger.info(argv[1] + " called with", argv.slice().join(" "));
+                // Log the input, but don't log passwords
+                logger.info(argv[1] + " called with", argv.slice().join(" "));
 
-
-            // Start processing...
-            q()
-                .then(function() {
-                    if (options.waitFor) {
-                        logger.info("Waiting for", options.waitFor);
-                        return ipc.once(options.waitFor);
-                    }
-                })
-                .then(function() {
-                    var deferred = q.defer();
-
-                    if (options.url) {
-                        util.download(options.url)
-                            .then(function(fileName) {
-                                options.file = fileName;
-                                fs.chmod(fileName, parseInt('0755',8), function() {
-                                    deferred.resolve();
-                                });
-                            })
-                            .catch(function(err) {
-                                deferred.reject(err);
-                            })
-                            .done();
-                    }
-                    else {
-                        deferred.resolve();
-                    }
-
-                    return deferred.promise;
-                })
-                .then(function() {
-                    var deferred = q.defer();
-                    var args = [];
-                    var cp_options = {};
-                    var cp;
-
-                    if (options.file) {
-                        logger.info("Custom script starting.");
-                        ipc.send(signals.SCRIPT_RUNNING);
-
-                        if (options.clArgs) {
-                            args = options.clArgs.split(/\s+/);
+                // Save args in restart script in case we need to reboot to recover from an error
+                util.saveArgs(argv, ARGS_FILE_ID)
+                    .then(function() {
+                        if (options.waitFor) {
+                            logger.info("Waiting for", options.waitFor);
+                            return ipc.once(options.waitFor);
                         }
+                    })
+                    .then(function() {
+                        var deferred = q.defer();
 
-                        if (options.cwd) {
-                            cp_options.cwd = options.cwd;
+                        if (options.url) {
+                            util.download(options.url)
+                                .then(function(fileName) {
+                                    options.file = fileName;
+                                    fs.chmod(fileName, parseInt('0755',8), function() {
+                                        deferred.resolve();
+                                    });
+                                })
+                                .catch(function(err) {
+                                    deferred.reject(err);
+                                })
+                                .done();
                         }
-
-                        cp = child_process.spawn(options.file, args, cp_options);
-
-                        cp.stdout.on('data', function(data) {
-                            logger.info(data.toString().trim());
-                        });
-
-                        cp.stderr.on('data', function(data) {
-                            logger.error(data.toString().trim());
-                        });
-
-                        cp.on('close', function(code) {
-                            logger.info('child process exited with code', code);
+                        else {
                             deferred.resolve();
-                        });
+                        }
+
+                        return deferred.promise;
+                    })
+                    .then(function() {
+                        var deferred = q.defer();
+                        var args = [];
+                        var cp_options = {};
+                        var cp;
+
+                        if (options.file) {
+                            logger.info("Custom script starting.");
+                            ipc.send(signals.SCRIPT_RUNNING);
+
+                            if (options.clArgs) {
+                                args = options.clArgs.split(/\s+/);
+                            }
+
+                            if (options.cwd) {
+                                cp_options.cwd = options.cwd;
+                            }
+
+                            cp = child_process.spawn(options.file, args, cp_options);
+
+                            cp.stdout.on('data', function(data) {
+                                logger.info(data.toString().trim());
+                            });
+
+                            cp.stderr.on('data', function(data) {
+                                logger.error(data.toString().trim());
+                            });
+
+                            cp.on('close', function(code) {
+                                logger.info('child process exited with code', code);
+                                deferred.resolve();
+                            });
+                        }
+                        else {
+                            deferred.resolve();
+                        }
+
+                        return deferred.promise;
+                    })
+                    .then(function(response) {
+                        logger.debug(response);
+                        ipc.send(options.signal || signals.SCRIPT_DONE);
+                    })
+                    .catch(function(err) {
+                        logger.error("Running custom script failed", err);
+                    })
+                    .done(function(response) {
+                        logger.debug(response);
+                        logger.info("Custom script finished.");
+
+                        util.deleteArgs(ARGS_FILE_ID);
+
+                        if (cb) {
+                            cb();
+                        }
+                    });
+                }
+                catch (err) {
+                    if (logger) {
+                        logger.error("Custom script error:", err);
                     }
                     else {
-                        deferred.resolve();
+                        console.log("Custom script error:", err);
                     }
-
-                    return deferred.promise;
-                })
-                .then(function(response) {
-                    logger.debug(response);
-                    ipc.send(options.signal || signals.SCRIPT_DONE);
-                })
-                .catch(function(err) {
-                    logger.error("Running custom script failed", err);
-                })
-                .done(function(response) {
-                    logger.debug(response);
-                    logger.info("Custom script finished.");
-
-                    if (cb) {
-                        cb();
-                    }
-                });
+                }
         },
 
         getOptions: function() {
