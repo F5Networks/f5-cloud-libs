149598f258f4508017e80fd02ad113979dd0dc05
Mike Levine <m.levine@f5.com>
2016-10-20 11:53:57 -0700

    Switch to MIT license

--- scripts/aws/getNameServer.sh
+++ scripts/aws/getNameServer.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright 2016 F5 Networks, Inc.
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
 # For DNS name servers, AWS uses the CIDR of an interface +2 on the last octet
 # To use this script, pass in the name of the external interface
 
--- test/scripts/onboardTests.js
+++ test/scripts/onboardTests.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright 2016 F5 Networks, Inc.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+'use strict';
+
 var onboard = require('../../scripts/onboard');
 var q = require('q');
 
