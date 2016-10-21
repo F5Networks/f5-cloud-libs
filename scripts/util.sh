5eb3e92ce824e80fec7a6ca15d67244b24dcd6ba
Mike Levine <m.levine@f5.com>
2016-10-21 08:44:07 -0700

    Switch to MIT license

--- scripts/util.sh
+++ scripts/util.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright 2016 F5 Networks, Inc.
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
 # generic init utils
 
 # Wait for process settings
