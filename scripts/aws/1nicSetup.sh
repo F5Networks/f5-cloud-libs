458e2fa679ae926b732010ed89ca27471803a24b
Mike Levine <m.levine@f5.com>
2016-10-27 13:04:02 -0700

    trivial

--- scripts/aws/1nicSetup.sh
+++ scripts/aws/1nicSetup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Copyright 2016 F5 Networks, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 USAGE_SHORT="Usage: $0"
 read -r -d '' USAGE_LONG << EOM
