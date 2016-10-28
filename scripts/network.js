d1333f61b5c5d727abb79c20786bca2db535f893
Mike Levine <m.levine@f5.com>
2016-10-28 14:26:10 -0700

    network basics

---
+++ scripts/network.js
@@ -0,0 +1,346 @@
+/**
+ * Copyright 2016 F5 Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 'use strict';
+
+(function() {
+
+    var options;
+    var runner;
+
+    module.exports = runner = {
+
+        /**
+         * Runs the network setup script
+         *
+         * @param {String[]} argv - The process arguments
+         * @param {Object}   testOpts - Options used during testing
+         * @param {Object}   testOpts.bigIp - BigIp object to use for testing
+         * @param {Function} cb - Optional cb to call when done
+         */
+        run: function(argv, testOpts, cb) {
+            var BigIp = require('../lib/bigIp');
+            var Logger = require('../lib/logger');
+            var ActiveError = require('../lib/activeError');
+            var ipc = require('../lib/ipc');
+            var signals = require('../lib/signals');
+            var util = require('../lib/util');
+            var loggerOptions = {};
+            var loggableArgs;
+            var logger;
+            var logFileName;
+            var bigIp;
+            var forceReboot;
+            var i;
+
+            var DEFAULT_LOG_FILE = '/tmp/network.log';
+            var ARGS_FILE_ID = 'network';
+            var KEYS_TO_MASK = ['-p', '--password', '--set-password', '--set-root-password'];
+            var REQUIRED_OPTIONS = ['host', 'user', 'password'];
+            var DEFAULT_CIDR = '/24';
+
+            options = require('./commonOptions');
+            testOpts = testOpts || {};
+
+            try {
+                options = options.getCommonOptions()
+                    .option('--single-nic', 'Set db variables for single NIC configuration.')
+                    .option('--multi-nic', 'Set db variables for multi NIC configuration.')
+                    .option('--default-gw <gateway_address>', 'Set default gateway to gateway_address')
+                    .option('--vlan <name, nic_number, [tag]>', 'Create vlan with name on nic_number. Optionally specify a tag. Values should be comma-separated.', util.csv, [])
+                    .option('--self-ip <name, ip_address, vlan_name>', 'Create self IP with name and ip_address on vlan. Values should be comma-separated. Default CIDR prefix is 24 if not specified', util.csv, [])
+                    .parse(argv);
+
+                loggerOptions.console = true;
+                loggerOptions.logLevel = options.logLevel;
+
+                if (options.output) {
+                    loggerOptions.fileName = options.output;
+                }
+
+                logger = Logger.getLogger(loggerOptions);
+                util.logger = logger;
+
+                for (i = 0; i < REQUIRED_OPTIONS.length; ++i) {
+                    if (!options[REQUIRED_OPTIONS[i]]) {
+                        logger.error(REQUIRED_OPTIONS[i], "is a required command line option.");
+                        return;
+                    }
+                }
+
+                // When running in cloud init, we need to exit so that cloud init can complete and
+                // allow the BIG-IP services to start
+                if (options.background) {
+                    logFileName = options.output || DEFAULT_LOG_FILE;
+                    logger.info("Spawning child process to do the work. Output will be in", logFileName);
+                    util.runInBackgroundAndExit(process, logFileName);
+                }
+
+                // Log the input, but don't log passwords
+                loggableArgs = argv.slice();
+                for (i = 0; i < loggableArgs.length; ++i) {
+                    if (KEYS_TO_MASK.indexOf(loggableArgs[i]) !== -1) {
+                        loggableArgs[i + 1] = "*******";
+                    }
+                }
+                logger.info(loggableArgs[1] + " called with", loggableArgs.join(' '));
+
+                if (options.singleNic && options.multiNic) {
+                    logger.error("Only one of single-nic or multi-nic can be specified.");
+                    return;
+                }
+
+                // Create the bigIp client object
+                bigIp = testOpts.bigIp || new BigIp(options.host,
+                                                    options.user,
+                                                    options.password,
+                                                    {
+                                                        port: options.port,
+                                                        logger: logger
+                                                    });
+
+                // Start processing...
+
+                // Save args in restart script in case we need to reboot to recover from an error
+                util.saveArgs(argv, ARGS_FILE_ID)
+                    .then(function() {
+                        if (options.waitFor) {
+                            logger.info("Waiting for", options.waitFor);
+                            return ipc.once(options.waitFor);
+                        }
+                    })
+                    .then(function() {
+                        logger.info("Network setup starting.");
+                        ipc.send(signals.NETWORK_RUNNING);
+
+                        logger.info("Waiting for BIG-IP to be ready.");
+                        return bigIp.ready();
+                    })
+                    .then(function() {
+                        logger.info("BIG-IP is ready.");
+
+                        if (options.multiNic) {
+                            logger.info("Setting multi NIC options.");
+                            return bigIp.modify(
+                                '/tm/sys/db/provision.1nic',
+                                {
+                                    value: 'forced_enable'
+                                }
+                            )
+                            .then(function(response) {
+                                logger.debug(response);
+
+                                return bigIp.modify(
+                                    '/tm/sys/db/provision.1nicautoconfig',
+                                    {
+                                        value: 'disable'
+                                    }
+                                );
+                            })
+                            .then(function(response) {
+                                logger.debug(response);
+
+                                logger.info("Restarting mcpd.");
+                                return bigIp.create(
+                                    '/tm/sys/service',
+                                    {
+                                        command: 'restart',
+                                        name: 'mcpd'
+                                    },
+                                    {
+                                        noWait: true
+                                    }
+                                );
+                            })
+                            .then(function(response) {
+                                logger.debug(response);
+
+                                logger.info("Waiting for BIG-IP to be ready after mcpd restart.");
+                                return bigIp.ready();
+                            });
+                        }
+                    })
+                    .then(function(response) {
+                        logger.debug(response);
+
+                        if (options.defaultGw) {
+                            logger.info("Setting default gateway " + options.defaultGw);
+                            return bigIp.create(
+                                '/tm/net/route',
+                                {
+                                    name: "default",
+                                    gw: options.defaultGw
+                                }
+                            );
+                        }
+                    })
+                    .then(function(response) {
+                        logger.debug(response);
+
+                        var promises = [];
+                        var vlanName;
+                        var nicName;
+                        var tag;
+                        var vlanBody;
+
+                        if (options.vlan) {
+                            options.vlan.forEach(function(vlan) {
+                                if (vlan.length < 2) {
+                                    logger.warn("Invalid vlan parameters. Must have at least 2 values. Use --help for description");
+                                    return;
+                                }
+
+                                vlanName = vlan[0];
+                                nicName = vlan[1];
+                                tag = vlan.length > 2 ? vlan[2] : undefined;
+
+                                vlanBody = {
+                                    name: vlanName,
+                                    interfaces: [
+                                        {
+                                            name: nicName,
+                                            tagged: tag ? true : false
+                                        }
+                                    ]
+                                };
+
+                                if (tag) {
+                                    vlanBody.tag = tag;
+                                }
+
+                                promises.push(
+                                    {
+                                        promise: bigIp.create,
+                                        arguments: [
+                                            '/tm/net/vlan',
+                                            vlanBody
+                                        ],
+                                        message: "Creating vlan " + vlanName + " on interface " + nicName + (tag ? " with tag " + tag : " untagged")
+                                    }
+                                );
+                            });
+                        }
+
+                        return util.callInSerial(bigIp, promises);
+                    }.bind(this))
+                    .then(function(response) {
+                        logger.debug(response);
+
+                        var promises = [];
+                        var name;
+                        var ipAddress;
+                        var vlan;
+
+                        if (options.selfIp) {
+                            options.selfIp.forEach(function(selfIp) {
+                                if (selfIp.length < 3) {
+                                    logger.warn("Invalid self-ip parameters. Must be 3 values. Use --help for description.");
+                                    return;
+                                }
+
+                                name = selfIp[0];
+                                ipAddress = selfIp[1];
+                                vlan = selfIp[2];
+
+                                if (ipAddress.indexOf('/') === -1) {
+                                    ipAddress += DEFAULT_CIDR;
+                                }
+
+                                promises.push(
+                                    {
+                                        promise: bigIp.create,
+                                        arguments: [
+                                            '/tm/net/self',
+                                            {
+                                                name: name,
+                                                address: ipAddress,
+                                                vlan: '/Common/' + vlan,
+                                                allowService: 'default'
+                                            }
+                                        ],
+                                        message: "Creating self IP " + name + " with address " + ipAddress + " on vlan " + vlan
+                                    }
+                                );
+                            });
+                        }
+
+                        return util.callInSerial(bigIp, promises);
+                    }.bind(this))
+                    .then(function(response) {
+                        logger.debug(response);
+                        logger.info("BIG-IP network setup complete.");
+                        return bigIp.rebootRequired();
+                    })
+                    .then(function(response) {
+                        if (response) {
+                            if (options.reboot) {
+                                logger.warn('Reboot required. Rebooting...');
+                                return bigIp.reboot();
+                            }
+                            else {
+                                logger.warn('Reboot required. Skipping due to --no-reboot option.');
+                            }
+                        }
+                    })
+                    .then(function(response) {
+                        logger.debug(response);
+                        ipc.send(options.signal || signals.NETWORK_DONE);
+                    })
+                    .catch(function(err) {
+                        logger.error("BIG-IP network setup failed", err.message);
+
+                        if (err instanceof ActiveError) {
+                            logger.warn("BIG-IP active check failed. Preparing for reboot.");
+                            forceReboot = util.prepareArgsForReboot();
+                        }
+                    })
+                    .done(function(response) {
+                        logger.debug(response);
+                        logger.info("Network setup finished.");
+
+                        if (forceReboot) {
+                            logger.warn("Rebooting.");
+                            bigIp.reboot();
+                        }
+                        else {
+                            util.deleteArgs(ARGS_FILE_ID);
+                        }
+
+                        if (cb) {
+                            cb();
+                        }
+                    });
+            }
+            catch (err) {
+                if (logger) {
+                    logger.error("Network setup error:", err);
+                }
+                else {
+                    console.log("Network setup error:", err);
+                }
+            }
+        },
+
+        getOptions: function() {
+            return options;
+        }
+    };
+
+    // If we're called from the command line, run
+    // This allows for test code to call us as a module
+    if (!module.parent) {
+        runner.run(process.argv);
+    }
+})();
