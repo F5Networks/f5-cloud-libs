32717b8dbef384d7bd6cc249df0606e5329349f9
Mike Levine <m.levine@f5.com>
2016-12-12 14:51:26 -0800

    Changed onboard set-password to update-user
    update-user is now used for setting passwords and creating
    new users.
    
    set-password is no longer an option.

--- test/lib/bigIpOnboardTests.js
+++ test/lib/bigIpOnboardTests.js
@@ -309,5 +309,78 @@ module.exports = {
                 test.done();
             });
         }
+    },
+
+    testUpdateUser: {
+        testCreate: function(test) {
+            icontrolMock.when(
+                'list',
+                '/tm/auth/user',
+                [
+                    {
+                        name: 'admin'
+                    }
+                ]
+            );
+            bigIp.onboard.updateUser('myUser', 'myPass', 'myRole')
+            .then(function() {
+                var userParams = icontrolMock.getRequest('create', '/tm/auth/user');
+                test.strictEqual(userParams.name, 'myUser');
+                test.strictEqual(userParams.password, 'myPass');
+                test.strictEqual(userParams["partition-access"]["all-partitions"].role, 'myRole');
+            })
+            .catch(function(err) {
+                test.ok(false, err.message);
+            })
+            .finally(function() {
+                test.done();
+            });
+        },
+
+        testCreateNoExistingUsers: function(test) {
+            icontrolMock.when(
+                'list',
+                '/tm/auth/user',
+                {}
+            );
+            bigIp.onboard.updateUser('myUser', 'myPass', 'myRole')
+            .then(function() {
+                var userParams = icontrolMock.getRequest('create', '/tm/auth/user');
+                test.strictEqual(userParams.name, 'myUser');
+                test.strictEqual(userParams.password, 'myPass');
+                test.strictEqual(userParams["partition-access"]["all-partitions"].role, 'myRole');
+            })
+            .catch(function(err) {
+                test.ok(false, err.message);
+            })
+            .finally(function() {
+                test.done();
+            });
+        },
+
+        testUpdate: function(test) {
+            icontrolMock.when(
+                'list',
+                '/tm/auth/user',
+                [
+                    {
+                        name: 'myUser'
+                    }
+                ]
+            );
+            bigIp.onboard.updateUser('myUser', 'myPass', 'myRole')
+            .then(function() {
+                var userParams = icontrolMock.getRequest('modify', '/tm/auth/user/myUser');
+                test.strictEqual(userParams.name, undefined);
+                test.strictEqual(userParams.password, 'myPass');
+                test.strictEqual(userParams["partition-access"], undefined);
+            })
+            .catch(function(err) {
+                test.ok(false, err.message);
+            })
+            .finally(function() {
+                test.done();
+            });
+        }
     }
 };
