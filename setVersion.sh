87eb26c36fb42b028c9350d48e7b6bd16e460885
Mike Levine <m.levine@f5.com>
2017-01-06 08:56:04 -0800

    Add version to non-common scripts

--- setVersion.sh
+++ setVersion.sh
@@ -13,6 +13,10 @@ fi
 
 sed $SED_ARGS "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"$1\"/" package.json
 sed $SED_ARGS "s/version\('[0-9]+\.[0-9]+\.[0-9]+'\)/version(\'$1\')/" scripts/commonOptions.js
+sed $SED_ARGS "s/version\('[0-9]+\.[0-9]+\.[0-9]+'\)/version(\'$1\')/" scripts/network.js
+sed $SED_ARGS "s/version\('[0-9]+\.[0-9]+\.[0-9]+'\)/version(\'$1\')/" scripts/runScript.js
 
 rm -f package.json.bak
 rm -f scripts/commonOptions.js.bak
+rm -f scripts/network.js.bak
+rm -f scripts/runScript.js.bak
