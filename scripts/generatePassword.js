951b68d7c34315282a1cfac35e7b695986a69a6b
Mike Levine <m.levine@f5.com>
2017-06-14 11:39:28 -0700

    bumped version

--- scripts/generatePassword.js
+++ scripts/generatePassword.js
@@ -34,7 +34,7 @@
             };
 
             options
-                .version('3.0.0')
+                .version('3.1.0')
                 .option('--length <password_length>', 'Length of password. Default 32.', 32)
                 .option('--file <path/to/file>', 'Location in which to store the password. Default log to console.')
                 .parse(argv);
