6390a1f8efbc47dd6dd049c647c296d3620946c4
Mike Levine <m.levine@f5.com>
2017-07-11 09:07:57 -0700

    delete hosts in device-trust-group

--- scripts/autoscale.js
+++ scripts/autoscale.js
@@ -607,6 +607,18 @@
                 }
             })
             .then(function() {
+                if (hasUcs) {
+                    // If we loaded a UCS, the device_trust_group may contain old hosts.
+                    // Wipe them out and they will be re-created.
+                    return bigIp.put(
+                        '/tm/cm/device-group/device_trust_group',
+                        {
+                            devices: []
+                        }
+                    );
+                }
+            })
+            .then(function() {
                 logger.info("Writing master file.");
                 return writeMasterFile(hasUcs);
             });
