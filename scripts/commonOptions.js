b0ecde5ca518f3047cc87c62679764028e668e67
Mike Levine <m.levine@f5.com>
2017-12-27 15:29:37 -0800

    bump version

--- scripts/commonOptions.js
+++ scripts/commonOptions.js
@@ -30,7 +30,7 @@ module.exports = {
         options.reboot = true;
         options.port = 443;
         return options
-            .version('3.5.0')
+            .version('3.5.2')
             .option('--host <ip_address>', 'BIG-IP management IP to which to send commands.')
             .option('-u, --user <user>', 'BIG-IP admin user name.')
             .option('-p, --password <password>', 'BIG-IP admin user password. Use this or --password-url')
