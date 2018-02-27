a0d19835bac6ff3c6ace155b095776b18e05d1eb
Mike Levine <m.levine@f5.com>
2018-02-27 08:58:14 -0800

    Sync with original v2.1.1

---
+++ scripts/aws/getNameServer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright 2016 F5 Networks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# For DNS name servers, AWS uses the CIDR of an interface +2 on the last octet
+# To use this script, pass in the name of the external interface
+
+INTERFACE=$1
+INTERFACE_MAC=`ifconfig ${INTERFACE} | egrep HWaddr | awk '{print tolower($5)}'`
+VPC_CIDR_BLOCK=`curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/${INTERFACE_MAC}/vpc-ipv4-cidr-block`
+VPC_NET=${VPC_CIDR_BLOCK%/*}
+NAME_SERVER=`echo ${VPC_NET} | awk -F. '{ printf "%d.%d.%d.%d", $1, $2, $3, $4+2 }'`
+echo $NAME_SERVER
