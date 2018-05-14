29c968bbebc819426a63f59a051fdf335b8b6870
Mike Levine <m.levine@f5.com>
2018-05-14 12:26:23 -0700

    bump version

--- scripts/commonOptions.js
+++ scripts/commonOptions.js
@@ -33,7 +33,7 @@ module.exports = {
 
         /* eslint-disable max-len */
         return options
-            .version('4.1.0')
+            .version('4.1.1')
             .option(
                 '--host <ip_address>',
                 'BIG-IP management IP to which to send commands.'
