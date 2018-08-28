db1306c7af23591ccc25a27e91f166e890471741
Garrett Dieckmann <g.dieckmann@f5.com>
2018-08-28 13:53:23 -0700

    metricsCollectorTests conform to style guide

--- test/lib/metricsCollectorTests.js
+++ test/lib/metricsCollectorTests.js
@@ -13,33 +13,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 'use strict';
 
-var uuid = require('uuid5');
-var httpUtil = require('../../../f5-cloud-libs').httpUtil;
-var metricsCollector = require('../../../f5-cloud-libs').metricsCollector;
+const uuid = require('uuid5');
+const httpUtil = require('../../../f5-cloud-libs').httpUtil;
+const metricsCollector = require('../../../f5-cloud-libs').metricsCollector;
 
-var calledBody;
+let calledBody;
 
 module.exports = {
-    setUp: function(callback) {
+    setUp(callback) {
         calledBody = '';
-        httpUtil.post = function(url, options) {
+        httpUtil.post = (url, options) => {
             calledBody = options.body;
         };
 
         callback();
     },
 
-    tearDown: function(callback) {
-        Object.keys(require.cache).forEach(function(key) {
+    tearDown(callback) {
+        Object.keys(require.cache).forEach((key) => {
             delete require.cache[key];
         });
         callback();
     },
 
-    testBasic: function(test) {
-        var metrics = {
+    testBasic(test) {
+        const metrics = {
             customerId: 'myCustomerId',
             action: 'myAction',
             templateName: 'myTemplateName',
@@ -52,26 +53,27 @@ module.exports = {
             cloudLibsVersion: 'myCloudLibsVersion'
         };
         metricsCollector.upload(metrics);
-        test.strictEqual(calledBody,
-            '&v=1' +
-            '&t=event&ec=run' +
-            '&tid=' + 'UA-47575237-11' +
-            '&cid=' + uuid(metrics.customerId) +
-            '&aiid=' + uuid(metrics.customerId) +
-            '&ea=' + metrics.action +
-            '&an=' + metrics.templateName +
-            '&aid=' + metrics.deploymentId +
-            '&av=' + metrics.templateVersion +
-            '&cn=' + metrics.cloudName +
-            '&cm=' + metrics.region +
-            '&cs=' + metrics.bigIpVersion +
-            '&ck=' + metrics.licenseType +
-            '&ds=' + metrics.cloudLibsVersion);
+
+        test.strictEqual(calledBody, `&v=1\
+&t=event&ec=run\
+&tid=UA-47575237-11\
+&cid=${uuid(metrics.customerId)}\
+&aiid=${uuid(metrics.customerId)}\
+&ea=${metrics.action}\
+&an=${metrics.templateName}\
+&aid=${metrics.deploymentId}\
+&av=${metrics.templateVersion}\
+&cn=${metrics.cloudName}\
+&cm=${metrics.region}\
+&cs=${metrics.bigIpVersion}\
+&ck=${metrics.licenseType}\
+&ds=${metrics.cloudLibsVersion}`);
+
         test.done();
     },
 
-    testNoCustomerId: function(test) {
-        var metrics = {
+    testNoCustomerId(test) {
+        const metrics = {
             action: 'myAction',
             templateName: 'myTemplateName',
             deploymentId: 'myDeploymentId',
@@ -83,44 +85,42 @@ module.exports = {
             cloudLibsVersion: 'myCloudLibsVersion'
         };
         metricsCollector.upload(metrics)
-            .then(function() {
+            .then(() => {
                 test.ok(false, 'should have rejected no customerId');
             })
-            .catch(function(err) {
+            .catch((err) => {
                 test.notStictEqual(err.message.indexOf('customer id'), -1);
             })
-            .finally(function() {
+            .finally(() => {
                 test.done();
             });
     },
 
-    testDataTruncated: function(test) {
-        var metrics = {
+    testDataTruncated(test) {
+        const metrics = {
             customerId: 'myCustomerId',
             region: '012345678901234567890123456789012345678901234567891'
         };
         metricsCollector.upload(metrics);
-        test.strictEqual(calledBody,
-            '&v=1' +
-            '&t=event&ec=run' +
-            '&tid=' + 'UA-47575237-11' +
-            '&cid=' + uuid(metrics.customerId) +
-            '&aiid=' + uuid(metrics.customerId) +
-            '&ea=unknown' +
-            '&cm=' + '01234567890123456789012345678901234567890123456789'
-        );
+        test.strictEqual(calledBody, `&v=1\
+&t=event&ec=run\
+&tid=UA-47575237-11\
+&cid=${uuid(metrics.customerId)}\
+&aiid=${uuid(metrics.customerId)}\
+&ea=unknown\
+&cm=01234567890123456789012345678901234567890123456789`);
         test.done();
     },
 
-    testSetLogger: (function(test) {
-        test.doesNotThrow(function() {
+    testSetLogger: ((test) => {
+        test.doesNotThrow(() => {
             metricsCollector.setLogger({});
         });
         test.done();
     }),
 
-    testSetLoggerOptions: function(test) {
-        test.doesNotThrow(function() {
+    testSetLoggerOptions(test) {
+        test.doesNotThrow(() => {
             metricsCollector.setLoggerOptions({});
         });
         test.done();
