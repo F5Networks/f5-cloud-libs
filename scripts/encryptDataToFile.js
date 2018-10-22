de51ad9ff4a18c7f514d36e249d5845c04c6ed66
Mike Levine <m.levine@f5.com>
2018-10-22 13:38:36 -0700

    bump version

--- scripts/encryptDataToFile.js
+++ scripts/encryptDataToFile.js
@@ -57,7 +57,7 @@ const KEYS = require('../lib/sharedConstants').KEYS;
 
                 // Can't use getCommonOptions here because we don't take host, user, password options
                 options
-                    .version('4.5.0-beta.4')
+                    .version('4.6.0-beta.1')
                     .option(
                         '--background',
                         'Spawn a background process to do the work. If you are running in cloud init, you probably want this option.'
--- scripts/generatePassword.js
+++ scripts/generatePassword.js
@@ -33,7 +33,7 @@ const KEYS = require('../lib/sharedConstants').KEYS;
             let passwordPromise;
 
             options
-                .version('4.5.0-beta.4')
+                .version('4.6.0-beta.1')
                 .option(
                     '--length <password_length>',
                     'Length of password. Default 32.',
