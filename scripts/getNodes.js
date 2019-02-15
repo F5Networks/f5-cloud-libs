eff7ec542ba324e6a6cec0c2b8a1e93410ea47f9
Mike Levine <m.levine@f5.com>
2019-02-15 09:38:51 -0800

    Allow product to be specified. Bump version

--- scripts/getNodes.js
+++ scripts/getNodes.js
@@ -52,7 +52,7 @@ const Logger = require('../lib/logger');
             try {
                 /* eslint-disable max-len */
                 options
-                    .version('4.7.0')
+                    .version('4.8.0-beta.3')
                     .option(
                         '--cloud <cloud_provider>',
                         'Cloud provider (aws | azure | etc.)'
