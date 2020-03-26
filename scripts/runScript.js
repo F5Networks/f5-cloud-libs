8e2528efd3c7089e2f9de5ae86d38cb625d3e692
Michael Shimkus <m.shimkus@f5.com>
2020-03-26 16:31:22 -0700

    non beta

--- scripts/runScript.js
+++ scripts/runScript.js
@@ -49,7 +49,7 @@ const util = require('../lib/util');
             try {
                 /* eslint-disable max-len */
                 options
-                    .version('4.17.0-beta.2')
+                    .version('4.17.0')
                     .option(
                         '--background',
                         'Spawn a background process to do the work. If you are running in cloud init, you probably want this option.'
