e1a2a90402bb83a43b59a3a72c1968a23f45781b
Greg Crosby <g.crosby@f5.com>
2020-04-07 18:42:28 -0700

    bump version

--- scripts/network.js
+++ scripts/network.js
@@ -70,7 +70,7 @@ const cryptoUtil = require('../lib/cryptoUtil');
 
                 // Can't use getCommonOptions here because of the special reboot handling
                 options
-                    .version('4.17.0')
+                    .version('4.17.1')
                     .option(
                         '--host <ip_address>',
                         'BIG-IP management IP to which to send commands.'
