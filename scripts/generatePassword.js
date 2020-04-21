a3637e91eb09ae0a89bd6eb315e3aad685a89741
Andrey KASHCHEEV <a.kashcheev@f5.com>
2020-04-21 11:11:41 -0700

    bump version

--- scripts/generatePassword.js
+++ scripts/generatePassword.js
@@ -33,7 +33,7 @@ const KEYS = require('../lib/sharedConstants').KEYS;
             let passwordPromise;
 
             options
-                .version('4.17.1')
+                .version('4.18.0')
                 .option(
                     '--length <password_length>',
                     'Length of password. Default 32.',
